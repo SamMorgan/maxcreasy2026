5c4b53d11609d5689a9cdd2248e1643b0f66dde5
Sam Morgan <sam@svmorgan.com>
2026-06-17 16:05:10 +0100

    simplified text editor

--- studio/src/schemaTypes/objects/blockContentTextOnly.tsx
+++ studio/src/schemaTypes/objects/blockContentTextOnly.tsx
@@ -7,6 +7,32 @@ export const blockContentTextOnly = defineType({
   of: [
     defineArrayMember({
       type: 'block',
+      styles: [],
+      lists: [], 
+      marks: {
+        decorators: [],
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'Link',
+            fields: [
+              defineField({
+                name: 'href',
+                title: 'URL',
+                type: 'string',
+                validation: (Rule) => Rule.required(),
+              }),
+              defineField({
+                name: 'openInNewTab',
+                title: 'Open in new tab',
+                type: 'boolean',
+                initialValue: false,
+              }),
+            ],
+          },
+        ],
+      },
     }),
   ],
 })
